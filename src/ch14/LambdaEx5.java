4cb4d4176520b6eb837910231416482f07f3922d
junnjuns <donamjun1@naver.com>
2023-07-06 14:45:00 +0900

    test: 람다식 배열 메서드 참조 테스트 추가

--- src/ch14/LambdaEx5.java
+++ src/ch14/LambdaEx5.java
@@ -6,7 +6,10 @@ public class LambdaEx5 {
     public static void main(String[] args) {
         Function<String,Integer> f = Integer::parseInt; //메서드 참조
         int num = f.apply("100");
-        System.out.println(num+200);
+        System.out.println(num+100);
 
+        Function<Integer,int[]> f2 = int[]::new;
+        int leng = f2.apply(100).length;
+        System.out.println("생성한 f2 배열의 길이: "+leng);
     }
 }
